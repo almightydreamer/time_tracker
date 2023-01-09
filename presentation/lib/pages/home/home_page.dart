a642ab2559575623a58efd68d19abe5e8ab1bc83
Alex <aleexxutzuu@yahoo.com>
2023-01-09 16:42:33 +0200

    IMPROVED HOME PAGE

--- presentation/lib/pages/home/home_page.dart
+++ presentation/lib/pages/home/home_page.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:domain/modules/home/action/entity/action_entity.dart';
 import 'package:domain/modules/home/activity/entity/activity_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:simple_timer/simple_timer.dart';
 import 'controllers/home_controller.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
@@ -14,13 +18,21 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  late TimerController _timerController;
+
   @override
   void initState() {
     Get.put(HomeController());
     HomeController controller = Get.find();
     controller.getActions();
     controller.resumeActivity();
+    _timerController = TimerController(this);
+    if (controller.currentActivity.value.actionId != 0) {
+      _timerController.start();
+      _timerController.forward(
+          from: controller.currentActivity.value.startOfActivity.second.toDouble() - DateTime.now().second);
+    }
     super.initState();
   }
 
@@ -29,9 +41,12 @@ class _HomePageState extends State<HomePage> {
     Get.put(HomeController());
     HomeController controller = Get.find();
     //controller.saveActions();
+    print(
+        'activity ${DateFormat.Hms().format(controller.currentActivity.value.startOfActivity)} '
+            '|| time ${DateFormat.Hms().format(DateTime.now()).toString()}');
     return Scaffold(
       body: Container(
-        color: Colors.white,
+        color: const Color(0xFF4e6151),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
@@ -50,26 +65,43 @@ class _HomePageState extends State<HomePage> {
                   },
                   itemBuilder: (BuildContext context, int index) {
                     return Padding(
-                      padding: EdgeInsets.symmetric(vertical: 3),
-                      child: Container(
-                        height: 50,
-                        width: 150,
-                        decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(12),
-                            border: Border.all(color: Colors.black)),
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 100,
-                              child: Draggable(
-                                data: controller.actions[index],
-                                childWhenDragging: Text("${controller.actions[index].name}"),
-                                feedback: Text("${controller.actions[index].name}"),
+                      padding: const EdgeInsets.symmetric(vertical: 3),
+                      child: Draggable(
+                        data: controller.actions[index],
+                        feedback: Container(
+                          height: 50,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              color: const Color(0xFF5e8c61),
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(color: Colors.black)),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 100,
                                 child: Text("${controller.actions[index].name}"),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
+                        ),
+                        childWhenDragging: Container(
+                          height: 50,
+                        ),
+                        child: Container(
+                          height: 50,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              color: const Color(0xFF5e8c61),
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(color: Colors.black)),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 100,
+                                child: Text(controller.actions[index].name),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     );
@@ -79,9 +111,17 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Container(
-              decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.only(topRight: Radius.circular(6), topLeft: Radius.circular(6))),
+              decoration: const BoxDecoration(
+                color: Color(0xFF5e8c61),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(32), topRight: Radius.circular(32)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFF3b322c),
+                    blurRadius: 0,
+                    offset: Offset(0, -3),
+                  ),
+                ],
+              ),
               width: MediaQuery.of(context).size.width,
               height: 200,
               child: DragTarget<ActionEntity>(onAccept: (ActionEntity action) {
@@ -93,12 +133,11 @@ class _HomePageState extends State<HomePage> {
                 ));
                 controller.isStarted.value = true;
                 controller.activeAction.value = action.name;
-                controller.currentActivity.value =
-                    ActivityEntity(day: DateTime.now().day, actionId: 0, startOfActivity: DateTime.now());
+                controller.currentActivity.value = ActivityEntity(
+                    day: DateTime.now().day, actionId: 0, startOfActivity: DateTime.now(), actionName: action.name);
                 triggerTimer();
               }, builder: (context, _, __) {
                 return Obx(() => Container(
-                      color: Colors.pinkAccent,
                       height: 200,
                       width: 200,
                       child: controller.activeAction.value == " "
@@ -106,13 +145,29 @@ class _HomePageState extends State<HomePage> {
                               color: Colors.cyan,
                               height: 100,
                               width: 100,
-                              child: Text("${controller.currentActivity.value.actionName} ${controller.isStarted.value}"),
+                              child: Center(
+                                  child: Text(
+                                      "${controller.currentActivity.value.actionName} ${controller.isStarted.value}")),
                             )
                           : Container(
-                              color: Colors.amber,
                               height: 100,
                               width: 100,
-                              child: Text("${controller.currentActivity.value.actionName} ${controller.isStarted.value}"),
+                              child: Center(
+                                  child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'You are currently',
+                                    style: const TextStyle(fontSize: 32),
+                                  ),
+                                  Text(
+                                    "${controller.currentActivity.value.actionName}",
+                                    style: const TextStyle(fontSize: 48),
+                                  ),
+                                  Text('for ${_timerController.duration}')
+                                ],
+                              )),
                             ),
                     ));
               }),
