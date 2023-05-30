50a2b080abd3aa16c6accd01c68fff98124ece0f
Alex <aleexxutzuu@yahoo.com>
2023-05-30 22:09:51 +0300

    added Composite

--- domain/lib/modules/home/activity/entity/activity_entity.dart
+++ domain/lib/modules/home/activity/entity/activity_entity.dart
@@ -3,22 +3,25 @@ import 'package:domain/modules/home/action/entity/action_entity.dart';
 ///
 /// TMPS Project
 ///
-/// This is an example of factory method
+/// This is an example of Factory Method
+/// This is an example of Composite
 ///
 
 class ActivityEntity extends ActionEntity {
   final int? id;
   final int actionId;
   final String actionName;
+  final List<ActivityEntity> subActivities;
   final int day;
   final DateTime startOfActivity;
   final DateTime? endOfActivity;
 
-   ActivityEntity({
+  ActivityEntity({
     this.id,
     required this.day,
     required this.actionId,
     required this.actionName,
+    this.subActivities = const [],
     this.endOfActivity,
     required this.startOfActivity,
   }) : super(id: actionId, name: actionName);
