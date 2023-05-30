6a6d8d6b34ddec535ad01d4c98100bb9688c455c
Alex <aleexxutzuu@yahoo.com>
2023-05-30 22:22:42 +0300

    added Chain of responsibility

--- domain/lib/modules/home/activity/usecase/get_activities_usecase.dart
+++ domain/lib/modules/home/activity/usecase/get_activities_usecase.dart
@@ -3,6 +3,14 @@ import 'package:either_dart/either.dart';
 import '../entity/activity_entity.dart';
 import '../repository/activity_repository.dart';
 
+///
+/// TMPS project
+///
+/// This is an example of Chain of responsibility
+///
+/// CTRL + Left click on [repository.getLocalActivityList] to see the continuation of the chain
+///
+
 class GetActivitiesUseCase {
   final ActivityRepository repository;
 
